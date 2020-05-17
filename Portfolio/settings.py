7a4097b517c27bb8ed25120f35fffae3e35c498e
suroor89 <bravalji@gmail.com>
2020-05-16 22:32:21 -0400

    updated settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aw$j_i+sp-+7$h0cis$(y^9+(&+k8lf9kaalo)cfhwuj1h^*4!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
